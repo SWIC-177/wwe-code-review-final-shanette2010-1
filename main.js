c5073d1919760b6d600d89ba7dc4989d58956ee3
Manav Misra <manavm@visionwebsoft.com>
2024-04-27 14:36:36 -0500

    refactor:

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import { CHAMPIONS } from "./src/data";
+import { getLastName } from "./src/lib";
 
 // Alphabetize CHAMPIONS by last name
-const sortedChampions = CHAMPIONS.toSorted();
+const sortedChampions = CHAMPIONS.toSorted((a, b) =>
+  getLastName(a).localeCompare(getLastName(b)),
+);
 
 console.log(sortedChampions);
